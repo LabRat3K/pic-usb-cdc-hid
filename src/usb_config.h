d5b432dea9dace99f53c605e336f2f6f74280510
LabRat3K <LabRat3K@users.noreply.github.com>
2023-02-23 17:57:04 -0800

    Change usb to FULL_SPEED (BD is then accepted by WIN10)

--- src/usb_config.h
+++ src/usb_config.h
@@ -74,8 +74,8 @@
 //  is available on the target processor.
 //#define USB_TRANSCEIVER_OPTION USB_EXTERNAL_TRANSCEIVER
 
-//#define USB_SPEED_OPTION USB_FULL_SPEED
-#define USB_SPEED_OPTION USB_LOW_SPEED //(not valid option for PIC24F devices)
+#define USB_SPEED_OPTION USB_FULL_SPEED
+//#define USB_SPEED_OPTION USB_LOW_SPEED //(not valid option for PIC24F devices)
 
 #define MY_VID 0x04D8
 #define MY_PID 0x0055
@@ -138,7 +138,7 @@
 
 /* HID */
 #define HID_INTF_ID             0x00
-#define HID_EP 					1
+#define HID_EP 			1
 #define HID_INT_OUT_EP_SIZE     1
 #define HID_INT_IN_EP_SIZE      8
 #define HID_NUM_OF_DSC          1
